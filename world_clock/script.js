2123e467987908474283e1f9b4debad95d48854c
LHK <lhk_lihongkai@163.com>
2023-12-14 22:06:11 +0800

    用户数据addedCities存储在本地并自动加载，添加了一些城市

--- world_clock/script.js
+++ world_clock/script.js
@@ -9,11 +9,13 @@
 
   // 从cities.json文件中获取城市列表
   var cities = [] //用于接收文件里的城市列表
-  let addedCities = []; // 用于存储已添加的城市名称
+  var addedCities = []; // 用于存储已添加的城市名称
 
   document.addEventListener('DOMContentLoaded', (event) => {
     fetch('./cities.json')
-      .then(response => response.json())
+    // 使用 then 方法指定第一个回调函数，用于处理响应对象
+      .then(response => response.json()) // 返回一个 Promise 对象，用于等待响应对象的 json 方法返回一个 JavaScript 对象
+      // 使用 then 方法指定第二个回调函数，用于处理 JavaScript 对象
       .then( data => {
         // 确保data是一个数组
         if (Array.isArray(cities)) {
@@ -23,6 +25,39 @@
           // data.forEach(city => { 
           //   console.log(city.name); 
           // });
+
+          //从本地存储中获取已添加的城市列表(用then方法，确保数据已经获取到)
+          const storedCities = localStorage.getItem('addedCities');
+          if (storedCities) {
+            addedCities = JSON.parse(storedCities);
+            // console.log(addedCities);
+            // console.log(cities);
+            addedCities.forEach(cityName => {
+              // console.log(cityName);
+              const city = cities.find(function(c){
+                return c.name === cityName;
+              });
+              // console.log(city);
+              // 加判断是为了防止cities.json文件中的城市列表发生变化，导致本地存储的addedCities在cities中找不到
+              if(city){displayCity(city.name, city.timezone, city.tzIdentifier);}
+              // 发现city不存在后删除本地存储中的城市名称(不运行下边这一段就只是不显示，但是本地存储中还是有的)
+              else {
+                addedCities.splice(addedCities.indexOf(cityName), 1); //删掉过时的城市名称并更新本地存储
+                localStorage.setItem('addedCities', JSON.stringify(addedCities));
+                //解决删除后造成的遍历移位问题，重新遍历一遍addedCities即可
+                addedCities.forEach(cityName => {
+                  // console.log(cityName);
+                  const city = cities.find(function(c){
+                    return c.name === cityName;
+                  });
+                  // console.log(city);
+                  if(city){displayCity(city.name, city.timezone, city.tzIdentifier);}
+                }
+                )
+              }
+            });
+          }
+
         } else {
           console.error('Data is not an array:', data);
         }
@@ -31,15 +66,30 @@
         console.error('Error fetching cities:', error);
       });
     
-    // // 从本地存储中获取已添加的城市列表（不能正常显示）
+
+    // // 从本地存储中获取已添加的城市列表
     // const storedCities = localStorage.getItem('addedCities');
     // if (storedCities) {
     //   addedCities = JSON.parse(storedCities);
+    //   console.log(addedCities);
+    //   console.log(cities);
     //   addedCities.forEach(cityName => {
-    //     const city = cities.find(c => c.name === cityName);
-    //     if (city) displayCity(city.name, city.timezone, city.tzIdentifier);
+    //     console.log(cityName);
+    //     const city = cities.find(function(c){
+    //       return c.name === cityName;
+    //     });
+    //     console.log(city);
+    //     // displayCity(city.name, city.timezone, city.tzIdentifier);
     //   });
     // }
+
+
+
+      // addedCities.forEach(cityName => {
+      //   const city = cities.find(c => c.name === cityName);
+      //   if (city) displayCity(city.name, city.timezone, city.tzIdentifier);
+      // });
+      updateLocalTime(); //打开后立即更新本地时间（防止隔1秒才出现造成卡顿效果）
   });
   
 
