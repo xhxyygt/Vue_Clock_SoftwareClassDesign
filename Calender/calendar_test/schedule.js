46cc16b736355a409555186611d4cd42597d7f62
LHK <lhk_lihongkai@163.com>
2024-01-03 20:25:00 +0800

    开始做农历

--- Calender/calendar_test/schedule.js
+++ Calender/calendar_test/schedule.js
@@ -1,80 +1,16 @@
-(function() {
-    angular
-      .module('calendarApp', ['ngAnimate'])
-      .controller('calendarController', calendarController);
-  
-    function calendarController($scope) {
-      var vm = this,
-        now = new Date(),
-        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        jan = daysInMonth(1, now.getFullYear()),
-        feb = daysInMonth(2, now.getFullYear()),
-        mar = daysInMonth(3, now.getFullYear()),
-        apr = daysInMonth(4, now.getFullYear()),
-        may = daysInMonth(5, now.getFullYear()),
-        jun = daysInMonth(6, now.getFullYear()),
-        jul = daysInMonth(7, now.getFullYear()),
-        aug = daysInMonth(8, now.getFullYear()),
-        sep = daysInMonth(9, now.getFullYear()),
-        oct = daysInMonth(10, now.getFullYear()),
-        nov = daysInMonth(11, now.getFullYear()),
-        dec = daysInMonth(12, now.getFullYear()),
-        monthRef = [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec],
-        month = now.getMonth(),
-        monthDay = monthRef[now.getMonth()],
-        n = now.getDate();
-  
-      vm.id = now.getDate().toString() + now.getMonth().toString();
-      vm.dataId;
-      vm.events = [];
-      vm.description;
-      vm.type = 'Social';
-      vm.month = months[month];
-      vm.next = next;
-      vm.prev = prev;
-      vm.add = add;
-        
-      // 突出今天
-      function presentDay() {
-        $(".date_item").eq(n - 1).addClass("present");
-      }
-  
-      // 打印当前月份的日期列表
-      function showDays(days) {
-        for (var i = 1; i < days; i++) {
-          var uidi = i;
-          var uidm = month;
-          var uid = uidi.toString() + uidm.toString();
-          $(".calendar-days").append("<div class='date_item' data='" + uid + "'>" + i + "</div>");
+// 用户点击日期的响应：改变背景颜色，显示当天的日程
+const calendarDays = document.querySelector('.calendar-days');
+calendarDays.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.classList.contains('date-item')) {
+        // 添加current-date类名，同时去掉其他元素的current-date类名
+        target.classList.add('current-date');
+        const siblings = target.parentNode.children;
+        for (let i = 0; i < siblings.length; i++) {
+            if (siblings[i] !== target) {
+                siblings[i].classList.remove('current-date');
+            }
         }
-      }
-
-  
-      // 将事件添加到指定日期
-      function add() {
-        vm.events.push({
-          id: vm.id,
-          description: vm.description,
-          type: vm.type
-        });
-  
-        vm.description = "";
-      }
-  
-      // 获取每个日期项的唯一ID
-      $(".calendar-days").on("click", ".date_item",function() {
-        vm.id = $(this).attr('data');
-        // vm.dataId = $(this).attr('data');
-        $(this).addClass("current-date").siblings().removeClass("current-date");
-        $scope.$apply();
-      });
-  
-      showDays(monthDay);
-  
-      presentDay();
-  
-      placeIt();
-  
     }
-  
-  })();
+}
+);
--- Calender/calendar_test/script.js
+++ Calender/calendar_test/script.js
@@ -1,3 +1,4 @@
+
 const isLeapYear = (year) => {
     return (
       (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
@@ -183,37 +184,3 @@ const isLeapYear = (year) => {
             generateCalendar(currentMonth.value, currentYear.value);
         }
     });
-
-    // const today = document.querySelector('.date-time-formate');
-    // today.addEventListener('click', () => {
-    //     currentMonth.value = currentDate.getMonth();
-    //     currentYear.value = currentDate.getFullYear();
-    //     generateCalendar(currentMonth.value, currentYear.value);
-    //     }
-    // );
-
-// 用户点击某一天后，当天的背景色变化
-    const calendarDays = document.querySelector('.calendar-days');
-    calendarDays.addEventListener('click', (e) => {
-        const target = e.target;
-        if (target.classList.contains('date-item')) {
-            // 添加current-date类名，同时去掉其他元素的current-date类名
-            target.classList.add('current-date');
-            const siblings = target.parentNode.children;
-            for (let i = 0; i < siblings.length; i++) {
-                if (siblings[i] !== target) {
-                    siblings[i].classList.remove('current-date');
-                }
-            }
-            
-
-        }
-
-        // target.addClass("current-date").siblings().removeClass("current-date");
-        // if (target.classList.contains('current-date')) {
-        //     target.classList.add('current-date-clicked');
-        // }
-    }
-    );
-
-
